9a61d0a133b4530602309cdb6ba4e83ff2228994
markbugatti <supermark11@gmail.com>
2019-03-12 16:33:31 +0200

    aplication urls fixed

--- lw1/urls.py
+++ lw1/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('Aplication0/', include('Aplication0.urls')),
+    # path('Aplication0/', include('Aplication0.urls')),
     path('Aplication1/', include('Aplication1.urls')),
+    path('', include('Aplication1.urls')),
     path('admin/', admin.site.urls),
 ]
